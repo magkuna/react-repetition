995e4504adff902bba5deddc849a43dc5248cf29
magkuna <magkuna@gmail.com>
2019-04-05 10:57:32 +0200

    playing with MUI changing olors in buttons

--- src/MUIButton.js
+++ src/MUIButton.js
@@ -1,23 +1,39 @@
-    
+
 import React from 'react'
 
 import Button from '@material-ui/core/Button'
+import { withStyles } from '@material-ui/core'
 
-const MUIButton = (props) => (
-  <div>
-    <Button
-    variant={'contained'}
-    color={'primary'}
-    >
-      MUIButton
-    </Button>
-    <Button
-    variant={'contained'}
-    color={'secondary'}
-    >
-      MUIButton
-    </Button>
-  </div>
+const styles = {
+    button: {
+        margin: 20
+    },
+    black: {
+        backgroundColor: 'black',
+        color: 'white'
+    }
+
+}
+
+const MUIButton = (props) => { 
+
+const { classes } = props
+return (
+    <div>
+        <Button
+            variant={'contained'}
+            className={classes.button + ' ' + classes.black}
+        >
+            MUIButton
+        </Button>
+        <Button
+            variant={'contained'}
+            color={'secondary'}
+        >
+            MUIButton
+        </Button>
+    </div>
 )
+}
 
-export default MUIButton
+export default withStyles(styles)(MUIButton)
